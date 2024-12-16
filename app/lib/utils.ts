44c94f16b640be1cf494c85d5fedd01d5856be36
Vini Fraga <vinifragam@gmail.com>
2024-12-16 14:51:28 -0300

    Criação de link

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -2,5 +2,14 @@ import { ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function sanitizeLink(link?: string) {
+  if (!link) return "";
+
+  return link
+    .replace(/\s/g, "")
+    .replace(/[!@#$%^&*()_+\-=\[\]{};':"\\|,ˆ.<>\/?]+/, "")
+    .toLocaleLowerCase();
 }
