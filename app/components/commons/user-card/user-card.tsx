0cc7e20a4f31d58d24dffe8f39289345151350d3
Vini Fraga <vinifragam@gmail.com>
2024-12-17 09:01:46 -0300

    Adicionando redes sociais

--- app/components/commons/user-card/user-card.tsx
+++ app/components/commons/user-card/user-card.tsx
@@ -1,8 +1,9 @@
 import { Github, Instagram, Linkedin, Plus, Twitter } from "lucide-react";
-import Button from "../ui/button";
+import EditSocialLinks from "./edit-social-links";
+import Button from "../../ui/button";
 
 export default function UserCard() {
-  const icons = [Github, Instagram, Linkedin, Twitter, Plus];
+  const icons = [Github, Instagram, Linkedin, Twitter];
 
   return (
     <div className="w-[348px] flex flex-col gap-5 items-center p-5 border border-white border-opacity-10 bg-[#121212] rounded-3xl text-white">
@@ -33,6 +34,7 @@ export default function UserCard() {
               <Icon />
             </button>
           ))}
+          <EditSocialLinks />
         </div>
       </div>
       <div className="flex flex-col gap-3 w-full h-[172px]">
