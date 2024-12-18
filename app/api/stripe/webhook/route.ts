fbff613130cc7c829cf856d6f26d468a85b6df82
Vini Fraga <vinifragam@gmail.com>
2024-12-18 17:28:10 -0300

    Envio de emails com Resend

--- app/api/stripe/webhook/route.ts
+++ app/api/stripe/webhook/route.ts
@@ -1,4 +1,5 @@
 import { db } from "@/app/lib/firebase";
+import { resend } from "@/app/lib/resend";
 import stripe from "@/app/lib/stripe";
 import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
@@ -46,6 +47,15 @@ export async function POST(req: NextRequest) {
           if (hostedVoucherUrl) {
             const userEmail = event.data.object.customer_details?.email;
             console.log("Enviar email para o cliente com o boleto");
+
+            if (userEmail) {
+              resend.emails.send({
+                from: "onboarding@resend.dev",
+                to: userEmail,
+                subject: "Seu boleto para pagamento",
+                text: `Aqui está o seu boleto: ${hostedVoucherUrl}`,
+              });
+            }
           }
         }
 
