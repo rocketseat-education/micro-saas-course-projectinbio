394cd2b2f20fa63f18ea09499381d51174d7fc03
Vini Fraga <vinifragam@gmail.com>
2024-12-18 11:47:34 -0300

    Ajustes no fluxo do usuário

--- app/components/landing-page/hero.tsx
+++ app/components/landing-page/hero.tsx
@@ -2,6 +2,7 @@ import ProjectCard from "../commons/project-card";
 import TotalVisits from "../commons/total-visits";
 import UserCard from "../commons/user-card/user-card";
 import Button from "../ui/button";
+import CreateNow from "../ui/create-now";
 import TextInput from "../ui/text-input";
 
 export default function Hero() {
@@ -16,11 +17,7 @@ export default function Hero() {
           <br />
           Acompanhe o engajamento com Analytics de cliques
         </h2>
-        <div className="flex items-center gap-2 w-full mt-[10vh]">
-          <span className="text-white text-xl">projectinbio.com</span>
-          <TextInput placeholder="Seu link" />
-          <Button>Criar agora</Button>
-        </div>
+        <CreateNow />
       </div>
 
       <div className="w-full flex items-center justify-center bg-[radial-gradient(circle_at_50%_50%,#4B2DBB,transparent_55%)]">
